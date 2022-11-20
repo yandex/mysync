944ec15f6bc0e12a61e603b038ad384fe3a0c8d5
Dmitry Smal <mialinx@gmail.com>
2022-11-21 01:26:02 +0300

    attempt to fix docker tests 2

--- tests/images/mysql/setup.sh
+++ tests/images/mysql/setup.sh
@@ -3,8 +3,6 @@ set -e
 chown mysql:root /etc/mysql
 touch /etc/mysync.yaml
 chown mysql:mysql /etc/mysync.yaml 
-mkdir /var/run/mysql
-chown mysql:root /var/run/mysql
 cp /var/lib/dist/mysql/my.cnf /etc/mysql/my.cnf
 cp /var/lib/dist/mysql/.my.cnf /root/.my.cnf
 cp /var/lib/dist/mysql/supervisor_mysql.conf /etc/supervisor/conf.d
