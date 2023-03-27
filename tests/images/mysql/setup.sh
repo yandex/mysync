ccde55e6c3848d1c3f218e06a18e54e980e665f5
suetin <suetin@yandex-team.ru>
2023-03-28 00:54:41 +0300

    fix config for 8.0

--- tests/images/mysql/setup.sh
+++ tests/images/mysql/setup.sh
@@ -6,17 +6,16 @@ chown mysql:mysql /etc/mysync.yaml
 if [[ "$VERSION" == "8-0" ]]; then
   cp /var/lib/dist/mysql/my.cnf.8.0 /etc/mysql/my.cnf
   cp /var/lib/dist/mysql/my.cnf.8.0 /etc/mysql/init.cnf
-else
-  cp /var/lib/dist/mysql/my.cnf /etc/mysql/my.cnf
-  cp /var/lib/dist/mysql/my.cnf /etc/mysql/init.cnf
-fi
-
 cat <<EOF >> /etc/mysql/my.cnf
 rpl_semi_sync_master_timeout = 31536000000
 rpl_semi_sync_master_wait_for_slave_count = 1
 rpl_semi_sync_master_wait_no_slave = ON
 rpl_semi_sync_master_wait_point = AFTER_SYNC
 EOF
+else
+  cp /var/lib/dist/mysql/my.cnf /etc/mysql/my.cnf
+  cp /var/lib/dist/mysql/my.cnf /etc/mysql/init.cnf
+fi
 
 cp /var/lib/dist/mysql/.my.cnf /root/.my.cnf
 cp /var/lib/dist/mysql/supervisor_mysql.conf /etc/supervisor/conf.d
