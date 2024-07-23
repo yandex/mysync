cbf8d4df6da340737ce8d26e32f1d6c149679982
secwall <secwall@yandex-team.ru>
2024-07-23 18:33:49 +0200

    Add random host provider for zk

--- internal/dcs/config.go
+++ internal/dcs/config.go
@@ -9,24 +9,35 @@ import (
 
 // ZookeeperConfig contains Zookeeper connection info
 type ZookeeperConfig struct {
-	Hostname              string        `config:"hostname" yaml:"hostname"`
-	SessionTimeout        time.Duration `config:"session_timeout" yaml:"session_timeout"`
-	Namespace             string        `config:"namespace,required"`
-	Hosts                 []string      `config:"hosts,required"`
-	BackoffInterval       time.Duration `config:"backoff_interval" yaml:"backoff_interval"`
-	BackoffRandFactor     float64       `config:"backoff_rand_factor" yaml:"backoff_rand_factor"`
-	BackoffMultiplier     float64       `config:"backoff_multiplier" yaml:"backoff_multiplier"`
-	BackoffMaxInterval    time.Duration `config:"backoff_max_interval" yaml:"backoff_max_interval"`
-	BackoffMaxElapsedTime time.Duration `config:"backoff_max_elapsed_time" yaml:"backoff_max_elapsed_time"`
-	BackoffMaxRetries     uint64        `config:"backoff_max_retries" yaml:"backoff_max_retries"`
-	Auth                  bool          `config:"auth" yaml:"auth"`
-	Username              string        `config:"username" yaml:"username"`
-	Password              string        `config:"password" yaml:"password"`
-	UseSSL                bool          `config:"use_ssl" yaml:"use_ssl"`
-	KeyFile               string        `config:"keyfile" yaml:"keyfile"`
-	CertFile              string        `config:"certfile" yaml:"certfile"`
-	CACert                string        `config:"ca_cert" yaml:"ca_cert"`
-	VerifyCerts           bool          `config:"verify_certs" yaml:"verify_certs"`
+	Hostname              string                   `config:"hostname" yaml:"hostname"`
+	SessionTimeout        time.Duration            `config:"session_timeout" yaml:"session_timeout"`
+	Namespace             string                   `config:"namespace,required"`
+	Hosts                 []string                 `config:"hosts,required"`
+	BackoffInterval       time.Duration            `config:"backoff_interval" yaml:"backoff_interval"`
+	BackoffRandFactor     float64                  `config:"backoff_rand_factor" yaml:"backoff_rand_factor"`
+	BackoffMultiplier     float64                  `config:"backoff_multiplier" yaml:"backoff_multiplier"`
+	BackoffMaxInterval    time.Duration            `config:"backoff_max_interval" yaml:"backoff_max_interval"`
+	BackoffMaxElapsedTime time.Duration            `config:"backoff_max_elapsed_time" yaml:"backoff_max_elapsed_time"`
+	BackoffMaxRetries     uint64                   `config:"backoff_max_retries" yaml:"backoff_max_retries"`
+	RandomHostProvider    RandomHostProviderConfig `config:"random_host_provider" yaml:"random_host_provider"`
+	Auth                  bool                     `config:"auth" yaml:"auth"`
+	Username              string                   `config:"username" yaml:"username"`
+	Password              string                   `config:"password" yaml:"password"`
+	UseSSL                bool                     `config:"use_ssl" yaml:"use_ssl"`
+	KeyFile               string                   `config:"keyfile" yaml:"keyfile"`
+	CertFile              string                   `config:"certfile" yaml:"certfile"`
+	CACert                string                   `config:"ca_cert" yaml:"ca_cert"`
+	VerifyCerts           bool                     `config:"verify_certs" yaml:"verify_certs"`
+}
+
+type RandomHostProviderConfig struct {
+	LookupTTL time.Duration `config:"lookup_ttl" yaml:"lookup_ttl"`
+}
+
+func DefaultRandomHostProviderConfig() RandomHostProviderConfig {
+	return RandomHostProviderConfig{
+		LookupTTL: 300 * time.Second,
+	}
 }
 
 // DefaultZookeeperConfig return default Zookeeper connection configuration
@@ -44,6 +55,7 @@ func DefaultZookeeperConfig() (ZookeeperConfig, error) {
 		BackoffMaxInterval:    backoff.DefaultMaxInterval,
 		BackoffMaxElapsedTime: backoff.DefaultMaxElapsedTime,
 		BackoffMaxRetries:     10,
+		RandomHostProvider:    DefaultRandomHostProviderConfig(),
 	}
 	return config, nil
 }
