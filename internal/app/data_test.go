22e085e12459cd71719b256d72b89b13d42e847b
noname0443 <noname342341@gmail.com>
2024-10-09 13:32:58 +0300

    Fix linter and add test for map

--- internal/app/data_test.go
+++ internal/app/data_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestStringerWorksOnNodeState(t *testing.T) {
 	ns := &NodeState{}
-	ns_str := fmt.Sprintf("%s", ns)
-	if ns_str != "<ping=ERR repl=??? sync=??? ro=false offline=false lag=0.00 du=??? cr=??? gtid=???>" {
+	nsStr := fmt.Sprintf("%v", ns)
+	if nsStr != "<ping=ERR repl=??? sync=??? ro=false offline=false lag=0.00 du=??? cr=??? gtid=???>" {
 		t.Errorf("%s", ns)
 	}
 
@@ -18,14 +18,26 @@ func TestStringerWorksOnNodeState(t *testing.T) {
 	ns.SlaveState = new(SlaveState)
 	ns.SlaveState.ExecutedGtidSet = "6DBC0B04-4B09-43DC-86CC-9AF852DED919:1-40"
 
-	ns_str = fmt.Sprintf("%s", ns)
-	if ns_str != "<ping=ERR repl= sync=??? ro=false offline=false lag=NaN du=??? cr=??? gtid=6DBC0B04-4B09-43DC-86CC-9AF852DED919:1-40>" {
+	nsStr = fmt.Sprintf("%v", ns)
+	if nsStr != "<ping=ERR repl= sync=??? ro=false offline=false lag=NaN du=??? cr=??? gtid=6DBC0B04-4B09-43DC-86CC-9AF852DED919:1-40>" {
 		t.Errorf("%s", ns)
 	}
 
 	ns.ShowOnlyGTIDDiff = true
-	ns_str = fmt.Sprintf("%s", ns)
-	if ns_str != "<ping=ERR repl= sync=??? ro=false offline=false lag=NaN du=??? cr=??? gtid=source ahead on: 6dbc0b04-4b09-43dc-86cc-9af852ded919:41-101>" {
+	nsStr = fmt.Sprintf("%v", ns)
+	if nsStr != "<ping=ERR repl= sync=??? ro=false offline=false lag=NaN du=??? cr=??? gtid=source ahead on: 6dbc0b04-4b09-43dc-86cc-9af852ded919:41-101>" {
 		t.Errorf("%s", ns)
+	}
+}
+
+func TestStringerWorksOnNodeStateMap(t *testing.T) {
+	m := make(map[string]*NodeState)
+	m["a"] = &NodeState{}
+	m["b"] = &NodeState{}
+	m["c"] = &NodeState{}
+
+	mStr := fmt.Sprintf("%v", m)
+	if mStr != "map[a:<ping=ERR repl=??? sync=??? ro=false offline=false lag=0.00 du=??? cr=??? gtid=???> b:<ping=ERR repl=??? sync=??? ro=false offline=false lag=0.00 du=??? cr=??? gtid=???> c:<ping=ERR repl=??? sync=??? ro=false offline=false lag=0.00 du=??? cr=??? gtid=???>]" {
+		t.Errorf("%s", mStr)
 	}
 }
