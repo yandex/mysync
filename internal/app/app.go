f74231df1ee4f9828b3e321c5af1096b0f08d097
withsoull <withsoull@yandex.ru>
2025-02-14 16:53:40 +0300

    fix

--- internal/app/app.go
+++ internal/app/app.go
@@ -768,18 +768,19 @@ func (app *App) checkMasterVisible(clusterStateFromDB, clusterStateDcs map[strin
 		app.logger.Errorf("checkMasterVisible: can`t get muster host, error: %s", err)
 		return false, err
 	}
+  state, ok := clusterStateFromDB[masterHost]
+  if ok && state.PingOk {
+    app.logger.Debug("Master is visible by manager, than we don`t need manager`s switchover")
+    return true, nil
+  }
 
-	if state, ok := clusterStateFromDB[masterHost]; !ok || !state.PingOk {
-		retryPingOk, err := app.cluster.PingNode(masterHost)
-		if err != nil {
-			app.logger.Errorf("checkMasterVisible: app.cluster.PingNode(%s) fail with error: %s", masterHost, err)
-			return retryPingOk, err
-		}
-	}
-
-	app.logger.Debug("Master is visible by manager, than we don`t need manager`s switchover")
-
-	return true, nil
+  retryPingOk, err := app.cluster.PingNode(masterHost)
+  if err != nil {
+    app.logger.Errorf("checkMasterVisible: app.cluster.PingNode(%s) fail with error: %s", masterHost, err)
+    return retryPingOk, err
+  } else {
+    return true, nil
+  }
 }
 
 func (app *App) checkQuorum(clusterStateFromDB, clusterStateDcs map[string]*NodeState) (appState, bool) {
