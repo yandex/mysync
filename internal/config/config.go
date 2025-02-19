013bea1edf618b67f00e46c63168827153c6bce3
noname0443 <noname342341@gmail.com>
2025-02-19 12:45:14 +0300

    change lines

--- internal/config/config.go
+++ internal/config/config.go
@@ -99,8 +99,8 @@ type Config struct {
 	ReplMonErrorWaitInterval                time.Duration                `config:"repl_mon_error_wait_interval" yaml:"repl_mon_error_wait_interval"`
 	ReplMonSlaveWaitInterval                time.Duration                `config:"repl_mon_slave_wait_interval" yaml:"repl_mon_slave_wait_interval"`
 	ShowOnlyGTIDDiff                        bool                         `config:"show_only_gtid_diff" yaml:"show_only_gtid_diff"`
-	ForceSwitchover                         bool                         `config:"force_switchover" yaml:"force_switchover"` // TODO: Remove when we will be sure it's right way to do switchover
 	ManagerSwitchover                       bool                         `config:"manager_switchover" yaml:"manager_switchover"`
+	ForceSwitchover                         bool                         `config:"force_switchover" yaml:"force_switchover"` // TODO: Remove when we will be sure it's right way to do switchover
 	OptimizeReplicationBeforeSwitchover     bool                         `config:"optimize_replication_before_switchover" yaml:"optimize_replication_before_switchover"` // TODO: Remove when we will be sure it's right way to do switchover
 	OptimizeReplicationLagThreshold         time.Duration                `config:"optimize_replication_lag_threshold" yaml:"optimize_replication_lag_threshold"`
 	OptimizeReplicationConvergenceTimeout   time.Duration                `config:"optimize_replication_convergence_timeout" yaml:"optimize_replication_convergence_timeout"`
