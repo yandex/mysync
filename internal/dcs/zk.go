4c1833afd789e8283198963e00b113e19d240ecb
munakoiso <ermilin.c@yandex.ru>
2025-04-22 23:06:51 +0500

    MDB-35448 do not fail getTree if data in zk-node is invalid json (#180)
    Co-authored-by: noname0443 <noname342341@gmail.com>

--- internal/dcs/zk.go
+++ internal/dcs/zk.go
@@ -509,8 +509,8 @@ func (z *zkDCS) GetTree(path string) (any, error) {
 		var ret any
 		err = json.Unmarshal(data, &ret)
 		if err != nil {
-			z.logger.Errorf("malformed node data %s (%s): %v", fullPath, data, err)
-			return nil, err
+			z.logger.Warnf("malformed node data %s (%s): %v", fullPath, data, err)
+			return string(data[:]), nil
 		}
 		return ret, nil
 	}
