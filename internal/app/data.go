de9f4795b4ff12f49fecdc6f5c8977329271b21b
teem0n <teem0n@yandex-team.ru>
2026-06-24 16:16:30 +0300

    app_files + app_dcs_paths added

--- internal/app/data.go
+++ internal/app/data.go
@@ -16,60 +16,6 @@ const (
 	stateMaintenance = "Maintenance"
 )
 
-const (
-	// manager's lock
-	pathManagerLock = "manager"
-
-	pathMasterNode = "master"
-
-	// def 1: activeNodes are master + alive running HA replicas ???
-	// def 2: active nodes is last iteration surely ok ones
-	// structure: list of hosts(strings)
-	pathActiveNodes = "active_nodes"
-
-	// structure: pathHealthPrefix/hostname -> NodeState
-	pathHealthPrefix = "health"
-
-	// structure: single Switchover
-	pathCurrentSwitch = "switch"
-
-	// structure: single Switchover
-	pathLastSwitch = "last_switch"
-
-	// structure: single Switchover
-	pathLastRejectedSwitch = "last_rejected_switch"
-
-	// structure: single Maintenance
-	pathMaintenance = "maintenance"
-
-	// structure: pathRecovery/hostname -> nil
-	pathRecovery = "recovery"
-
-	// List of HA nodes. May be modified by external tools (e.g. remove node from HA-cluster)
-	// list of strings
-	pathHANodes = "ha_nodes"
-
-	// List of Cascade nodes. May be modified by external tools (e.g. on node removal)
-	// structure: pathCascadeNodesPrefix/hostname -> CascadeNodeConfiguration
-	pathCascadeNodesPrefix = "cascade_nodes"
-
-	// low space flag
-	// structure: single value boolean
-	pathLowSpace = "low_space"
-
-	// resetup status
-	// structure: pathResetupStatus/hostname -> ResetupStatus
-	pathResetupStatus = "resetup_status"
-
-	pathLastShutdownNodeTime = "last_shutdown_node_time"
-
-	// last known timestamp from repl_mon table
-	pathMasterReplMonTS = "master_repl_mon_ts"
-
-	// timing start timestamps, structure: pathTimings/<name> -> time.Time
-	pathTimings = "timing"
-)
-
 var (
 	ErrNoMaster      = errors.New("no alive master found")
 	ErrManyMasters   = errors.New("more than one master found")
